47bc9ca2fed2ae86b32fea764ad5d28482634835
ibkkkkk <ruiifg40@gmail.com>
2022-12-12 11:06:06 +0900

    PORT

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 require("dotenv").config();
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 const app = express();
 const path = require("path");
 const cors = require("cors");
